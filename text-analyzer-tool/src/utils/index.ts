dab4c2f3198451861080027da59152e5770b324f
zhanyang01 <ngzhanyang01@gmail.com>
2024-06-30 15:01:43 +0800

    chore add comments for regex

--- text-analyzer-tool/src/utils/index.ts
+++ text-analyzer-tool/src/utils/index.ts
@@ -1,5 +1,13 @@
 import { pronouns } from "../data/pronouns"
 
+// this is also used in counting pronouns and finding longest word as we need to find words
+// first i split based on words with a regex /\b[w']+\b/g
+// what the regex do is 
+// /b beginning word boundary
+// [w'] => check for any word characters and '
+// + is to add
+// \b ending word boundary
+// /g check for all words not just the first word
 export const countWords = (content: string) => {
     const contentArray = content.split(/\b[\w']+\b/g)
     const words = contentArray.filter((word) => word !== "")
@@ -10,11 +18,17 @@ export const countCharacters = (content: string) => {
     return content.length
 }
 
+// we split based on sentences which is basically
+// (?<=[.!?]) positive lookbehind, basically check whether there is a . or ? or ! in front
+// \s matches for whitespaces
+// + is to add
+// (?=[A-Z]) -> this is to check positive lookahead which is to check whether it starts with a capital letter
 export const countSentences = (content : string) => {
     const sentences = content.split(/(?<=[.!?])\s+(?=[A-Z])/)
     return sentences.length
 }
 
+// i split based on newline which is \n
 export const countParagraphs = (content : string) => {
     const paragraphs = content.split(/\n/)
     const filteredParagraphs = paragraphs.filter((paragraph) => paragraph.trim().length > 0)
